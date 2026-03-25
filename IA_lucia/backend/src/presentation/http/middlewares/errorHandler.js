1e26c05b76427f00fc5f2316c97400308ddd7f18
Joaquim <bjoaquim242@gmail.com>
2026-03-25 16:18:56 -0300

    irei refazer tudo de novo

--- IA_lucia/backend/src/presentation/http/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export function errorHandler(error, _req, res, _next) {
-  const isClientError = /required|invalid|not found/i.test(error.message);
-  const statusCode = isClientError ? 400 : 500;
-
-  res.status(statusCode).json({
-    error: error.message,
-  });
-}
