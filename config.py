46fbd02a18f882b73a15ee975666bc723baf292b
Joaquim <bjoaquim242@gmail.com>
2026-03-26 15:37:58 -0300

    fix bugs for env and database

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-class Settings:
-    PORT: int = int(os.getenv("PORT", 8000))
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "secret")
-    ALGORITHM: str = "HS256"
-
-    def get_origins(self):
-        return ["*"]
-
-
-settings = Settings()
+DATABASE_URL = os.getenv("DATABASE_URL")
